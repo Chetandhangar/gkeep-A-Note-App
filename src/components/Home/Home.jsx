84946c620c98713d3e349c30f2229d21f80abdc2
Chetan Dhangar <chetansdhangar@gmail.com>
2021-08-23 10:38:30 +0530

    test - setting up the components

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+//import {} from 'react'
+
+import {Note} from '../Note/Note'
+
+export const Home = () => {
+    return(
+        <div>
+            Home Component
+            <Note />
+        </div>
+    )
+}
---
+++ src/components/Note/Note.jsx
@@ -0,0 +1,8 @@
+
+export const Note = () => {
+        return(
+            <div>
+                RenderNOte
+            </div>
+        )
+}
